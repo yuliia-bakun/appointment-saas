aec174fde6c6d40714b9162f360f8c7fa9d01cc5
ybakun <ybakun@student.codam.nl>
2026-09-09 16:45:14 +0200

    add prisma, change node version to 24, move env.example

---
+++ srcs/backend/prisma/migrations/20260909141227_add_test_table/migration.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "Test" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Test_pkey" PRIMARY KEY ("id")
+);
